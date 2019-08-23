fe401c15a52d3e68494cb62d70447bbaff83c2e7
Sanu Ann <sanuann@mit.edu>
2019-08-23 09:51:53 -0400

    change font size of text in footer

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer>
     <div>
-      voice-up is a demo and not an actual study.
+      Voice-up is a demo and not an actual study.
     </div>
   </footer>
 </template>
@@ -16,33 +16,18 @@ export default {
 footer {
   align-items: baseline;
   background-color: #219c6d;
-  color: black;
+  color: floralwhite;
   display: flex;
   flex-wrap: wrap;
-  font-size: .6rem;
+  font-size: 2rem;
   justify-content: space-around;
-  padding: .25rem .75rem .5rem .75rem;
+  padding: .25rem .75rem .25rem .75rem;
   position: fixed;
   left: 0;
   bottom: 0;
   width: 100%;
-}
-footer > div {
-  display: flex;
-  align-items: baseline;
-  justify-content: space-around;
   text-align: center;
 }
-a {
-  color: black;
-}
-footer > div:first-child > * {
-  padding: .5vw;
-  line-height: 1.3;
-}
-@media screen and (max-width: 50em) {
-  footer {
-    font-size: 3vw;
-  }
-}
+
+
 </style>
