d16b8b99ec99f106027952492ec9f36ef80aece9
Sanu Ann <sanuann@mit.edu>
2020-01-31 15:33:14 -0500

    revert deploy changes

--- deploy.sh
+++ deploy.sh
@@ -1,62 +1,36 @@
-##!/bin/bash
-#
-#set -e # exit with nonzero exit code if anything fails
-#
-#if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
-#
-#echo "Starting to update gh-pages\n"
-#
-##copy data we're interested in to other place
-#cp -R dist $HOME/dist
-#
-##go to home and setup git
-#cd $HOME
-#git config --global user.email "travis@travis-ci.org"
-#git config --global user.name "Travis"
-#
-##using token clone gh-pages branch
-#git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git gh-pages > /dev/null
-#
-##go into directory and copy data we're interested in to that directory
-#cd gh-pages
-#cp -Rf $HOME/dist/* .
-#
-#echo "Allow files with underscore https://help.github.com/articles/files-that-start-with-an-underscore-are-missing/" > .nojekyll
-#echo "[View live](https://${GH_USER}.github.io/${GH_REPO}/)" > README.md
-#
-##add, commit and push files
-#git add -f .
-#git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
-#git push -fq origin gh-pages > /dev/null
-#
-#echo "Done updating gh-pages\n"
-#
-#else
-# echo "Skipped updating gh-pages, because build is not triggered from the master branch."
-#fi;
+#!/bin/bash
 
-#!/usr/bin/env sh
+set -e # exit with nonzero exit code if anything fails
 
-# abort on errors
-set -e
+if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
 
-# build
-npm run build
+echo "Starting to update gh-pages\n"
 
-# navigate into the build output directory
-cd dist
+#copy data we're interested in to other place
+cp -R dist $HOME/dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+#go to home and setup git
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis"
 
-git init
-git add -A
-git commit -m 'deploy'
+#using token clone gh-pages branch
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git gh-pages > /dev/null
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+#go into directory and copy data we're interested in to that directory
+cd gh-pages
+cp -Rf $HOME/dist/* .
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:sensein/voice-study.git master:gh-pages
+echo "Allow files with underscore https://help.github.com/articles/files-that-start-with-an-underscore-are-missing/" > .nojekyll
+echo "[View live](https://${GH_USER}.github.io/${GH_REPO}/)" > README.md
 
-cd -
+#add, commit and push files
+git add -f .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
+git push -fq origin gh-pages > /dev/null
+
+echo "Done updating gh-pages\n"
+
+else
+ echo "Skipped updating gh-pages, because build is not triggered from the master branch."
+fi;
